535440ea91bbdef8410d021e23fdfbb4dfe4b8e1
dessinemoiunprogramme <dessinemoiunprogramme@gmail.com>
2021-05-21 17:24:32 +0200

    Disable angular on generated static site

--- scully.scully-playground.config.ts
+++ scully.scully-playground.config.ts
@@ -1,15 +1,25 @@
-import { ScullyConfig } from '@scullyio/scully';
+import { ScullyConfig, setPluginConfig } from '@scullyio/scully';
+const { DisableAngular } = require('scully-plugin-disable-angular');
+
+const postRenderers = [ DisableAngular ];
+
+setPluginConfig(DisableAngular, 'render', {
+  removeState: true,
+});
+
 export const config: ScullyConfig = {
   projectRoot: "./src",
   projectName: "scully-playground",
   outDir: './dist/static',
+  defaultPostRenderers: postRenderers,
   routes: {
     '/:id': {
       type: 'json',
       id: {
           url: 'http://localhost:3000/articles',
           property: 'id'
-      }
-  }
+      },
+      postRenderers: postRenderers
+    }
   }
 };
